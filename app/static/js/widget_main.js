6b65940857b4ec1ee56ae67c24237f99f7ae0095
lark-web <larkmailinbox@gmail.com>
2017-02-17 17:27:15 +0300

    вставил виджет от воронина заработал но криво

---
+++ app/static/js/widget_main.js
@@ -0,0 +1,247 @@
+
+
+    var widgetOptions = {
+        token: 'C584BE97-A4BE-4D53-99DF-4625FB00742D',
+        target: 'viz',
+        language: 'ru',
+        popupHeader: 'Византия',
+        maxAdults: 10,
+        widget_width: 928,
+        align: 'left',
+        showGuestsCount: true,
+        showChildren: true,
+        minAge: 1,
+        maxAge: 17,
+        method: 'popup',
+        showAllIfNotExists: true, 
+        usePaygate: false,
+        defLength: 1,
+        defAdults: 2,
+        alwaysShowTariff: false,
+        expanded: false,
+        showHours: false,
+        buttonText: 'Проверить наличие',
+        periodText: 'Выберите дату',
+        allowOverbooking: false,
+        emailRequired: false,
+        card: true,
+        popupHeader: 'Византия'
+    };
+
+    
+createWidget(widgetOptions);
+
+
+    widgetOptions = {
+        token: '3EEB66E7-F189-4553-8B7A-0D3D29A50033',
+        target: 'is',
+	language: 'ru',
+        popupHeader: 'Что это 1',
+        maxAdults: 10,
+        widget_width: 928,
+        align: 'left',
+        showGuestsCount: true,
+        showChildren: true,
+        minAge: 1,
+        maxAge: 17,
+        method: 'popup',
+        showAllIfNotExists: true, 
+        usePaygate: false,
+        defLength: 1,
+        defAdults: 2,
+        alwaysShowTariff: false,
+        expanded: false,
+        showHours: false,
+        buttonText: 'Проверить наличие',
+        periodText: 'Выберите дату',
+        allowOverbooking: false,
+        emailRequired: false,
+        card: true,
+        popupHeader: 'Исидор'
+    };
+
+    
+
+    createWidget(widgetOptions);
+
+widgetOptions = {
+        token: '432CFA38-34F6-4FF1-8C1D-59199908AE41',
+        target: 'hr',
+	language: 'ru',
+        popupHeader: 'Христакис',
+        maxAdults: 10,
+        widget_width: 928,
+        align: 'left',
+        showGuestsCount: true,
+        showChildren: true,
+        minAge: 1,
+        maxAge: 17,
+        method: 'popup',
+        showAllIfNotExists: true, 
+        usePaygate: false,
+        defLength: 1,
+        defAdults: 2,
+        alwaysShowTariff: false,
+        expanded: false,
+        showHours: false,
+        buttonText: 'Проверить наличие',
+        periodText: 'Выберите дату',
+        allowOverbooking: false,
+        emailRequired: false,
+        card: true,
+        popupHeader: 'Христакис'
+    };
+
+    
+
+    createWidget(widgetOptions);
+
+widgetOptions = {
+        token: '648AC3CC-982B-46E1-805B-CFEDF3F8BD35',
+        target: 'ro',
+	language: 'ru',
+        popupHeader: 'Родничок',
+        maxAdults: 10,
+        widget_width: 928,
+        align: 'left',
+        showGuestsCount: true,
+        showChildren: true,
+        minAge: 1,
+        maxAge: 17,
+        method: 'popup',
+        showAllIfNotExists: true, 
+        usePaygate: false,
+        defLength: 1,
+        defAdults: 2,
+        alwaysShowTariff: false,
+        expanded: false,
+        showHours: false,
+        buttonText: 'Проверить наличие',
+        periodText: 'Выберите дату',
+        allowOverbooking: false,
+        emailRequired: false,
+        card: true,
+        popupHeader: 'Родничок'
+    };
+
+    
+
+    createWidget(widgetOptions);
+
+widgetOptions = {
+        token: '4DBE3152-9CC5-41E2-9324-8F8F9A2C9292',
+        target: 'pr',
+	language: 'ru',
+        popupHeader: 'Приморский',
+        maxAdults: 10,
+        widget_width: 928,
+        align: 'left',
+        showGuestsCount: true,
+        showChildren: true,
+        minAge: 1,
+        maxAge: 17,
+        method: 'popup',
+        showAllIfNotExists: true, 
+        usePaygate: false,
+        defLength: 1,
+        defAdults: 2,
+        alwaysShowTariff: false,
+        expanded: false,
+        showHours: false,
+        buttonText: 'Проверить наличие',
+        periodText: 'Выберите дату',
+        allowOverbooking: false,
+        emailRequired: false,
+        card: true,
+        popupHeader: 'Приморский'
+    };
+
+    
+    createWidget(widgetOptions);
+
+widgetOptions = {
+        token: '5AC2A793-9873-4D05-8D65-92197C328F18',
+        target: 'yu',
+	language: 'ru',
+        popupHeader: 'Южная ночь',
+        maxAdults: 10,
+        widget_width: 928,
+        align: 'left',
+        showGuestsCount: true,
+        showChildren: true,
+        minAge: 1,
+        maxAge: 17,
+        method: 'popup',
+        showAllIfNotExists: true, 
+        usePaygate: false,
+        defLength: 1,
+        defAdults: 2,
+        alwaysShowTariff: false,
+        expanded: false,
+        showHours: false,
+        buttonText: 'Проверить наличие',
+        periodText: 'Выберите дату',
+        allowOverbooking: false,
+        emailRequired: false,
+        card: true,
+        popupHeader: 'Южная ночь'
+    };
+
+    
+    createWidget(widgetOptions);
+
+widgetOptions = {
+        token: '45E87E85-E82B-491F-A39F-C376DF50D4DD',
+        target: 'po',
+	language: 'ru',
+        popupHeader: 'Что это 2',
+        maxAdults: 10,
+        widget_width: 928,
+        align: 'left',
+        showGuestsCount: true,
+        showChildren: true,
+        minAge: 1,
+        maxAge: 17,
+        method: 'popup',
+        showAllIfNotExists: true, 
+        usePaygate: false,
+        defLength: 1,
+        defAdults: 2,
+        alwaysShowTariff: false,
+        expanded: false,
+        showHours: false,
+        buttonText: 'Проверить наличие',
+        periodText: 'Выберите дату',
+        allowOverbooking: false,
+        emailRequired: false,
+        card: true,
+        popupHeader: 'Посейдон'
+    };
+
+    
+    createWidget(widgetOptions);
+
+
+
+
+//$(document).ready(function(){
+//        $('select#sel-main').(function () {
+//        alert ('Попали в SELECT');
+//    
+//  });
+//});
+
+
+
+//document.getElementById('viz').style.display = 'none';
+
+//var divId = this.options[this.selectedIndex].value; 
+
+//            document.getElementById('viz').style.display = 'none'; document.getElementById('vizplace-holder').style.display = 'none'; 
+//            document.getElementById('is').style.display = 'none'; document.getElementById('isplace-holder').style.display = 'none'; 
+//            document.getElementById('hr').style.display = 'none'; document.getElementById('hrplace-holder').style.display = 'none';
+//            document.getElementById('ro').style.display = 'none'; document.getElementById('roplace-holder').style.display = 'none';
+//            document.getElementById('pr').style.display = 'none'; document.getElementById('prplace-holder').style.display = 'none'; 
+//            document.getElementById('yu').style.display = 'none'; document.getElementById('yuplace-holder').style.display = 'none';
+//            document.getElementById('po').style.display = 'none'; document.getElementById('poplace-holder').style.display = 'none';
+//            document.getElementById(divId).style.display = ''; document.getElementById(divId + 'place-holder').style.display = '';
